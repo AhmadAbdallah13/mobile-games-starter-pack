d26329c1e19b588cc93eb22e18418a69c2dc8a5b
Ahmad Abdallah <ahmad@sitech.local>
2024-05-22 20:30:48 +0300

    redirect to the arena scene correctly

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -1,10 +1,4 @@
 extends Control
 
-
 func _on_arena_btn_pressed():
-	# go the arena scene.
-	
-	pass
-	#globals.start_game.emit()
-	#await get_tree().create_timer(.3).timeout
-	#hide()
+	get_tree().call_group("ui", "go_to_arena")
--- Scripts/UI/ui.gd
+++ Scripts/UI/ui.gd
@@ -3,6 +3,7 @@ class_name UI
 
 @onready var main_menu = $MainMenu
 @onready var game_over_screen = $GameOverScreen
+@onready var arena = $Arena
 
 func show_game_over_screen():
 	show()
@@ -11,3 +12,7 @@ func show_game_over_screen():
 
 func _on_main_menu_start_game():
 	globals.start_game.emit()
+
+func go_to_arena():
+	main_menu.visible = false
+	arena.visible = true
