5a497abedfffe86edde06cd050a843c0af474c68
AhmadAbdallah13 <a7mtoo133@gmail.com>
2024-06-29 13:54:02 +0300

    add sounds settings and buses with a default sound for now

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _on_arena_btn_pressed():
 	get_tree().call_group("ui", "go_to_arena")
+
+
+func _on_settings_pressed() -> void:
+	get_tree().call_group("ui", "show_settings_pop_up")
--- Scripts/UI/ui.gd
+++ Scripts/UI/ui.gd
@@ -4,6 +4,7 @@ class_name UI
 @onready var main_menu = $MainMenu
 @onready var game_over_screen = $GameOverScreen
 @onready var arena = $Arena
+@onready var settings: Control = $Settings
 
 func show_game_over_screen():
 	show()
@@ -16,3 +17,6 @@ func _on_main_menu_start_game():
 func go_to_arena():
 	main_menu.visible = false
 	arena.visible = true
+
+func show_settings_pop_up():
+	settings.visible = true
