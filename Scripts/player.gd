e0a92b54b6db5723cfceea863fe3b9be4c5fed19
AhmadAbdallah13 <a7mtoo133@gmail.com>
2024-07-07 16:38:01 +0300

    [not successful] try to add velocity to the player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,6 +2,7 @@ class_name Player extends CharacterBody2D
 
 const SWIPE_LENGTH = 64
 const LANE_DIFF_LENGTH = 192
+
 var swiping = false
 
 var player_allowed_positions : Array
@@ -9,7 +10,19 @@ var player_allowed_positions : Array
 var finger_current_position: Vector2
 var finger_start_position: Vector2
 
+# How fast the player moves in meters per second.
+@export var speed = 14
+
 @export var player_health = 2
+
+#func _ready() -> void:
+	#velocity.y += speed
+
+func _process(delta):
+	print(velocity.y)
+	velocity.y += speed * delta
+	player_move_gesture()
+	move_and_collide(velocity * delta, true)
 
 func player_move_gesture():
 	if Input.is_action_just_pressed("SwipeGesture"):
@@ -25,16 +38,12 @@ func player_move_gesture():
 					swiping = false
 	else:
 		swiping = false
-		self.velocity.x = 0
+		velocity.x = 0
 
 func move_player():
 	var direction = 192 if finger_current_position.x > finger_start_position.x else -192
-	var new_position = int(self.position.x + direction)
-	self.position.x = new_position if new_position in player_allowed_positions else int(self.position.x)
-
-func _process(delta):
-	player_move_gesture()
-	move_and_collide(velocity * delta, true)
+	var new_position = int(position.x + direction)
+	position.x = new_position if new_position in player_allowed_positions else int(position.x)
 
 func _on_player_collision_detection_body_entered(body :Node2D):
 	if body is Car:
